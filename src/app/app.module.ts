6533247e166ea28a51406ef1a73e497082ce10c2
groovinflannel <groovinflannel@gmail.com>
2023-09-12 11:08:28 -0400

    added routing to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
