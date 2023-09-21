c560f6c1a5e86be7c05271973e45c42152f3750b
groovinflannel <groovinflannel@gmail.com>
2023-09-21 14:03:38 -0400

    added files to turn svg world map into Angular component

---
+++ src/app/map/worldMapSvg/worldMapSvg.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'worldMapSvg',
+  templateUrl: '../../assets/world.svg',
+  styleUrls: ['./worldMapSvg.component.css']
+})
+export class WorldMapSvg {
+  
+
+  
+}
